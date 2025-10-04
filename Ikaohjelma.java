a914858246c9720507eccc1b00a199831662a769
UnfairCooler <risto.kangas@hotmail.com>
2025-10-04 15:38:44 +0300

    Luotu Ikäohjelma joka kertoo mitä kukakin saa tai voi tehdä.

--- Ikaohjelma.java
+++ Ikaohjelma.java
@@ -1,7 +1,7 @@
 public class Ikaohjelma {
     public static void main(String[] args) throws Exception {
 
-        int ika = 18;
+        int ika = 100;
 
         if (ika > 0 && ika < 18) {
             System.out.println("Olet alaikäinen");
@@ -25,7 +25,23 @@ public class Ikaohjelma {
 
             if (ika >= 40 && ika <= 50) {
                 System.out.println("Parasta keski-ikää!");
+            }
 
+        } else if (ika >= 65) {
+            System.out.println("Olet eläkeläinen");
+
+            if (ika > 58 && ika < 65) {
+                System.out.println("Voit mennä varhaiseläkkeelle");
+            }
+
+            if (ika == 65) {
+                System.out.println("Hyviä eläkepäiviä!");
+            }
+
+            if (ika == 100) {
+                System.out.println("Paljon onnea 100-vuotiaalle!");
+                System.out.println("Olet saavuttanut hienon virstanpylvään.");
+                System.out.println("Hyvää juhlapäivää!");
             }
         }
     }
