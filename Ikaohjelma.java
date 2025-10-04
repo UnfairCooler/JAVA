04b72bfa032148e1103dbbd7a2764a0214966690
UnfairCooler <risto.kangas@hotmail.com>
2025-10-04 15:33:53 +0300

    Lisäilty vaihtoehtoja

--- Ikaohjelma.java
+++ Ikaohjelma.java
@@ -1,7 +1,7 @@
 public class Ikaohjelma {
     public static void main(String[] args) throws Exception {
 
-        int ika = 16;
+        int ika = 18;
 
         if (ika > 0 && ika < 18) {
             System.out.println("Olet alaikäinen");
@@ -11,6 +11,20 @@ public class Ikaohjelma {
             }
             if (ika == 16 || ika == 17) {
                 System.out.println("Saat ajaa kevaria");
+                }
+        } else if (ika == 18) { 
+            System.out.println("Olet juuri tullut täysi-ikäiseksi!");
+            System.out.println("Saat ajaa autoa");
+
+        } else if (ika >= 19 && ika < 65) {
+            System.out.println("Olet aikuinen");
+
+            if (ika % 10 == 0) {
+                System.out.println("Onnea " + ika + "-vuotispäivän johdosta!");
+            }
+
+            if (ika >= 40 && ika <= 50) {
+                System.out.println("Parasta keski-ikää!");
 
             }
         }
